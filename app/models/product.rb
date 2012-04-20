21a209b54e083c92aec78f1fd22139e887dd4f35
Maxim Filimonov <tpaktopsp@gmail.com>
2012-04-20 04:34:48 +0000

    9 finished. playtime

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,16 @@ class Product < ActiveRecord::Base
   }
   validates_length_of :description, maximum: 80
   attr_accessible :description, :image_url, :price, :title
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+  private
+    def ensure_not_referenced_by_any_line_item
+      if line_items.empty?
+        return true
+      else
+        errors.add(:base,'Line items present')
+        return false
+      end
+    end
 end
