e7ca223ba712469c09ebf67f582269232b39dfea
Maxim Filimonov <tpaktopsp@gmail.com>
2012-04-22 09:49:28 +0000

    Playtime

--- test/unit/cart_test.rb
+++ test/unit/cart_test.rb
@@ -4,4 +4,11 @@ class CartTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "add the duplicate products should result in quantity raise" do
+    cart = Cart.create
+    cart.add_product(line_items(:ruby).product_id).save
+    cart.add_product(line_items(:ruby).product_id).save
+    assert_equal 1,cart.line_items.length
+    assert_equal 2,cart.line_items.first.quantity
+  end
 end
